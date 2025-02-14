7d1bdc3289de8a1b051258fdb17e7c721a078c80
sellasouls666 <pgbc303@gmail.com>
2025-02-14 09:03:29 +0300

    Создал класс Group

---
+++ уп01/studentattendancelog/Model/Group.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace studentattendancelog.Model
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public required string GroupName { get; set; }
+        public List<Student> Students = new List<Student>();
+
+    }
+}
