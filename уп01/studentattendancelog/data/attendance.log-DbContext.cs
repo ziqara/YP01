12f2183b359b1cc9e1a78bbdd5ac3519af95630f
ziqara <165387888+ziqara@users.noreply.github.com>
2025-02-15 01:16:27 +0300

    Строка подключения, DBcontext

---
+++ уп01/studentattendancelog/data/attendance.log-DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using studentattendancelog.Model;
+
+namespace studentattendancelog.data
+{
+    public class attendance_log : DbContext
+    {
+        public attendance_log(DbContextOptions<attendance_log> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
