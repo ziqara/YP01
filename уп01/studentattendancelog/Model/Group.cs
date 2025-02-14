34ac25f8abdc28d73f503b2e1934f893a0c7df03
sellasouls666 <pgbc303@gmail.com>
2025-02-14 09:14:54 +0300

    Добавил поле Course в класс Group

--- уп01/studentattendancelog/Model/Group.cs
+++ уп01/studentattendancelog/Model/Group.cs
@@ -7,6 +7,6 @@ namespace studentattendancelog.Model
         public int GroupId { get; set; }
         public required string GroupName { get; set; }
         public List<Student> Students = new List<Student>();
-
+        public required Course Course { get; set; }
     }
 }
