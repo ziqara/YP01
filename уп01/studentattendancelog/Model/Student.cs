26a79e599df5c180745447bf09df8e2e362333c3
sellasouls666 <pgbc303@gmail.com>
2025-02-14 09:08:44 +0300

    Добавил поле Group в класс Student

--- уп01/studentattendancelog/Model/Student.cs
+++ уп01/studentattendancelog/Model/Student.cs
@@ -6,7 +6,7 @@
         public required string FirstName { get; set;}
         public required string LastName { get; set;}
         public required string MiddleName { get; set;}
-
+        public required Group Group { get; set;}
         public required string Email { get; set;}
         public required string PhoneNumber { get; set;}
     }
