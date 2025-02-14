0bb6102c7d47d6db49ad55da4cf0a78e3c5f25f3
sellasouls666 <pgbc303@gmail.com>
2025-02-14 09:13:19 +0300

    Создал класс Course

---
+++ уп01/studentattendancelog/Model/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace studentattendancelog.Model
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public required string CourseName { get; set; }
+        public required string Description { get; set; }
+        public List<Group> Groups = new List<Group>();
+
+    }
+}
