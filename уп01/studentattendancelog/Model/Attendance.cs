620ce885ffce476977df6a6d1a5212914bed4c94
wormgod <pgbc303@gmail.com>
2025-02-15 00:43:03 +0300

    Доделал класс Attendance

--- уп01/studentattendancelog/Model/Attendance.cs
+++ уп01/studentattendancelog/Model/Attendance.cs
@@ -2,5 +2,9 @@
 {
     public class Attendance
     {
+        public int AttendanceId { get; set; }
+        public Lesson Lesson { get; set; }
+        public Student Student { get; set; }
+        public enum AttendanceStatus { Присутствовал, Отсутствовал, Опоздал }
     }
 }
