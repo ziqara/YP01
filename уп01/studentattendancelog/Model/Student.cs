a9c7449570288ceb51f3436b6aebbb2748fda08f
sellasouls666 <pgbc303@gmail.com>
2025-02-14 08:56:43 +0300

    Создал класс Student

---
+++ уп01/studentattendancelog/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace studentattendancelog.Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public required string FirstName { get; set;}
+        public required string LastName { get; set;}
+        public required string MiddleName { get; set;}
+
+        public required string Email { get; set;}
+        public required string PhoneNumber { get; set;}
+    }
+}
